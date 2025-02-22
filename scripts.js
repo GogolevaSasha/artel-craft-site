ff25d742abb7b331942ac8588ad47ea38130ca58
GogolevaSasha <105587408+GogolevaSasha@users.noreply.github.com>
2025-02-22 19:32:04 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,14 +1,19 @@
-// scripts.js
+document.addEventListener("DOMContentLoaded", function () {
+    const menuButton = document.querySelector(".menu-button");
+    const closeButton = document.querySelector(".close-menu");
+    const sidebar = document.querySelector(".sidebar");
 
-// Открытие и закрытие бокового меню
-const menuButton = document.querySelector(".menu-button");
-const closeMenu = document.querySelector(".close-menu");
-const sidebar = document.querySelector(".sidebar");
+    menuButton.addEventListener("click", function () {
+        sidebar.classList.add("open");
+    });
 
-menuButton.addEventListener("click", function () {
-    sidebar.classList.add("open");
-});
+    closeButton.addEventListener("click", function () {
+        sidebar.classList.remove("open");
+    });
 
-closeMenu.addEventListener("click", function () {
-    sidebar.classList.remove("open");
+    document.addEventListener("click", function (event) {
+        if (!sidebar.contains(event.target) && !menuButton.contains(event.target)) {
+            sidebar.classList.remove("open");
+        }
+    });
 });
