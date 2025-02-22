a91446795210c76740eae3709bbe9350b6e686f4
GogolevaSasha <105587408+GogolevaSasha@users.noreply.github.com>
2025-02-22 19:19:05 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -12,19 +12,3 @@ menuButton.addEventListener("click", function () {
 closeMenu.addEventListener("click", function () {
     sidebar.classList.remove("open");
 });
-
-// Переключение вкладок на странице компании
-document.addEventListener("DOMContentLoaded", function () {
-    const tabs = document.querySelectorAll(".tab");
-    const tabContents = document.querySelectorAll(".tab-content");
-
-    tabs.forEach(tab => {
-        tab.addEventListener("click", function () {
-            tabs.forEach(t => t.classList.remove("active"));
-            tabContents.forEach(content => content.classList.remove("active"));
-
-            this.classList.add("active");
-            document.getElementById(this.dataset.tab).classList.add("active");
-        });
-    });
-});
